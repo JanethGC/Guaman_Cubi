ad1a371a0d2cdb4339c359b4fb76e6fec149c046
JanethGC <janethstefaniagc@gmail.com>
2023-01-29 13:48:10 -0500

    Conexion a la base de datos local

---
+++ products-api-company/src/database.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1/companydb",{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+})
+    .then( db => console.log(" DB is connected"))
+    .catch( error => console.log(error))
